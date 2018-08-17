1b58fbbf1783bb30f66e4c68b97ab2cec6df5971
Stig Rune Sellevag <stig-rune.sellevag@ffi.no>
2018-08-17 13:37:17 +0200

    Add test_matrix4

---
+++ tests/test_matrix4.cpp
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2018 Stig Rune Sellevag. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <matrix/matrix.h>
+#include <catch/catch.hpp>
+#include <iostream>
+
+
+TEST_CASE("test_matrix4")
+{
+    using namespace numlib;
+
+    Matrix<int, 4> m4(2, 3, 4, 5);
+
+    SECTION("size") { CHECK(m4.size() == 120); }
+
+    SECTION("extents")
+    {
+        CHECK(m4.extent(0) == 2);
+        CHECK(m4.extent(1) == 3);
+        CHECK(m4.extent(2) == 4);
+        CHECK(m4.extent(3) == 5);
+    }
+
+    SECTION("subscripting")
+    {
+        m4(0, 0, 0, 0) = 1;
+        m4(0, 0, 0, 1) = 2;
+
+        CHECK(m4(0, 0, 0, 0) == 1);
+        CHECK(m4(0, 0, 0, 1) == 2);
+    }
+}
